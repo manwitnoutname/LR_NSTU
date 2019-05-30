be6d5abf7256ca467aedfef54203e086bcb3ed08
manwitnoutname <fominykh.tolik@gmail.com>
2019-05-30 21:21:21 +0700

    add ids and interface IStorage, make out for MemCache and something else

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Lr1WebApi.Models;
+using ISWebApp.Storage;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lr1WebApi.Controllers
@@ -12,20 +13,21 @@ namespace Lr1WebApi.Controllers
     [Produces("application/json")]
     public class PersonController : ControllerBase
     {
-        private static List<PersonModel> persons = new List<PersonModel>();
+        private static IStorage<PersonModel> _memCache = new MemCache();
 
         [HttpGet]
         public ActionResult<IEnumerable<PersonModel>> Get()
         {
-            return Ok(persons) ;
+            return Ok(_memCache.All) ;
         }
 
         [HttpGet("{id}")]
-        public ActionResult<PersonModel> Get(int id)
+        public ActionResult<PersonModel> Get(Guid id)
         {
-            if (persons.Count <= id) return NotFound("Человека с таким номером анкеты нет!");
+            if (!_memCache.Has(id)) 
+            return NotFound("Человека с таким номером анкеты нет!");
         
-            return Ok(persons[id]);
+            return Ok(_memCache[id]);
         }
 
 
@@ -36,31 +38,31 @@ namespace Lr1WebApi.Controllers
 
             if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
             
-                persons.Add(value);
+                _memCache.Add(value);
 
                 return Ok($"{value.ToString() } Has been added");
         
         }
 
         [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromBody] PersonModel value)
+        public IActionResult Put(Guid id, [FromBody] PersonModel value)
         {
-            if (persons.Count <= id) return NotFound("Человека с таким номером анкеты нет!");
+            if (!_memCache.Has(id)) return NotFound("Человека с таким номером анкеты нет!");
             
             var validationResult =value.Validate();
             if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
-            var previousValue= persons[id]; 
-            persons[id] = value;
+            var previousValue= _memCache[id]; 
+            _memCache[id] = value;
 
             return Ok($"{previousValue.ToString()} has been updated to {value.ToString()}");
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
+        public IActionResult Delete(Guid id)
         {
-            if (persons.Count <= id) return NotFound("Человека с таким номером анкеты нет!");
-            var valueToRemove = persons[id];
-            persons.RemoveAt(id);
+            if (!_memCache.Has(id)) return NotFound("Человека с таким номером анкеты нет!");
+            var valueToRemove = _memCache[id];
+            _memCache.RemoveAt(id);
             return Ok($"{valueToRemove.ToString()} has been removed");
         }
     }
